1079ce0c5d470f60dd3b762f804b6b17ef68734d
Benedikt <b.bassewitz@icloud.com>
2026-06-01 17:29:57 +0200

    Design polish pass: self-hosted Inter font (47KB, privacy-safe), bolder hero/headings hierarchy, focus-visible a11y states, font smoothing + preload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'flippd-v1.8';
+const CACHE_NAME = 'flippd-v1.9';
 const ASSETS = [
   './',
   './index.html',
@@ -6,7 +6,8 @@ const ASSETS = [
   './icons/icon-192.png',
   './icons/icon-512.png',
   './assets/icons.css',
-  './assets/fonts/tabler-icons.woff2'
+  './assets/fonts/tabler-icons.woff2',
+  './assets/fonts/inter-latin.woff2'
 ];
 
 // Install: cache alle Assets
